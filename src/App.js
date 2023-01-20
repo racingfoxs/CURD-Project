70da6a3a85062b82936c9ae3ebc7414cd21faa44
DR_GURUNG <racingfoxs@gmail.com>
2023-01-20 19:34:52 +0530

    Local storage impleted

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Todo from "./components/Todo";
+import AddTodo from "./components/AddTodo";
 
 const App = () => {
-
-  const [todo, setTodo] = useState([
-   {
-    sno: 1,
-    title: "Go to Shop",
-    desc: "Bring Chocolate"
-  },
-  {
-    sno: 2,
-    title: "Go to Bakery",
-    desc: "Bring Cake"
-  },
-  {
-    sno: 3,
-    title: "Go to PetrolPump",
-    desc: "Petrol Pump"
+  let initTodo;
+  if (localStorage.getItem("todo")===null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todo"));
   }
-  ]);
 
-  const onDelete = todos => {
-    console.log("Works", todos);
-    setTodo(todo.filter((x) => x !== todos)); 
-};
+  const onDelete = (todos) => {
+    setTodo(
+      todo.filter((x) => {
+        return x !== todos;
+      })
+    );
+    localStorage.setItem(" todo", JSON.stringify(todo));
+  };
+
+  const addTodo = (title, desc) => {
+    let sno;
+    if (todo == 0) {
+      sno = 1;
+    } else {
+      sno = todo[todo.length - 1].sno + 1;
+    }
+
+    const newTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+
+    setTodo([...todo, newTodo]);
+  };
+
+  const [todo, setTodo] = useState(initTodo);
+
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(todo));
+  }, [todo]);
 
   return (
     <>
-      <Header/>
-      <Todo todo={todo} onDelete={onDelete}/>
-      <Footer/>
+      <Header />
+      <section className="bg-grey-500 text-black-600 px-20 py-10">
+        <AddTodo addTodo={addTodo} />
+      </section>
+      <Todo todo={todo} onDelete={onDelete} />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
